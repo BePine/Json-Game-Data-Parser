7bc8c9309640ef1ca131e9837a3a416957b4e82d
BePine <BePine17@gmail.com>
2024-06-21 16:14:52 +0200

    added fields

--- Json Game Data Parser/App/GameDataParser.cs
+++ Json Game Data Parser/App/GameDataParser.cs
@@ -14,15 +14,15 @@ namespace Json_Game_Data_Parser.App
         {
             Console.WriteLine("enter the name of the file you want to read:");
             var usersInput = _userInteraction.ReadInput();
-            _userInteraction.ShowInput();
+            _userInteraction.ShowInput(usersInput);
             Console.ReadKey();
 
         }
     }
     public interface IUserInteraction
     {
-        public string ReadInput();
-        public void ShowInput();
+        public IEnumerable<GameData> ReadInput();
+        public void ShowInput(IEnumerable<GameData> gamesData);
     }
 
     public class UserInteraction : IUserInteraction
@@ -32,37 +32,39 @@ namespace Json_Game_Data_Parser.App
         {
             _fileInteraction = fileInteraction;
         }
-        public string ReadInput()
+        public IEnumerable<GameData> ReadInput()
         {
-            var usersInput = Console.ReadLine();
+            string usersInput = Console.ReadLine();
 
             bool isValid = _fileInteraction.Validate(usersInput);
             if (isValid)
             {
-                _fileInteraction.Read(usersInput);
+                var gamesData = _fileInteraction.Read(usersInput);
+                return gamesData;
             }
-            else if(usersInput is null)
-            {
-                
-            }
-            return usersInput;
+            return null;
         }
-        public void ShowInput()
+        public void ShowInput(IEnumerable<GameData> gamesData)
         {
-
+            for (int i = 0; i < gamesData.Count(); i++)
+            {
+                var index = i + 1;
+                Console.WriteLine($"{index}. {gamesData.ElementAt(i).Title}, " +
+                    $" release: {gamesData.ElementAt(i).ReleaseYear}, rating: {gamesData.ElementAt(i).Rating}");
+            }
         }
     }
     public interface IFileInteraction
     {
         public bool Validate(string input);
-        public void Read(string usersInput);
+        public IEnumerable<GameData> Read(string usersInput);
     }
 
     public class FileInteraction : IFileInteraction
     {
         public bool Validate(string input)
         {
-            if (input is not null)
+            if (input != null)
             {
                 if (File.Exists(input))
                 {
@@ -72,10 +74,28 @@ namespace Json_Game_Data_Parser.App
             return false;
 
         }
-        public void Read(string usersInput)
+        public IEnumerable<GameData> Read(string usersInput)
         {
-            
+            IEnumerable<GameData> gamesData = new List<GameData>();
+
+            var fileContents = File.ReadAllText(usersInput);
+            gamesData = JsonSerializer.Deserialize<List<GameData>>(fileContents);
+            return gamesData;
+
         }
+    }
+    public interface IGameData
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; } 
+    }
+
+    public class GameData : IGameData
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
     }
 
     
